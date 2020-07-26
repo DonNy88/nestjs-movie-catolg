fb24a3f7ebc1d12848bb81dada1af3c6f9c14ce0
Donald Emeka Achugo <achugo.donald@gmail.com>
2020-07-27 00:21:13 +0200

    feat(dto): added Dto class

---
+++ src/common/dto/ResponseDto.ts
@@ -0,0 +1,16 @@
+import Dto from './Dto';
+import { HttpStatus } from '@nestjs/common';
+
+export default class RespnseDto extends Dto {
+  constructor({
+    statusCode = HttpStatus.OK,
+    message = 'OK',
+    data,
+  }: {
+    statusCode?: number;
+    message?: string;
+    data?: any;
+  }) {
+    super(statusCode, message, data, null);
+  }
+}
