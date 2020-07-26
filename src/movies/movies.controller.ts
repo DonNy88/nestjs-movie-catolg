f6479fad0750089009b73062908251a3a3a2da29
Donald Emeka Achugo <achugo.donald@gmail.com>
2020-07-27 00:25:18 +0200

    feat: wrapped all Movies endpoint wirh ResponseDto

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,36 +6,55 @@ import {
   Body,
   Put,
   Delete,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { Movie } from './movie.entity';
 import { MoviesService } from './movies.service';
+import RespnseDto from '../common/dto/ResponseDto';
 
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly movieService: MoviesService) {}
 
   @Get('/all')
-  async fetchAllMovies(): Promise<Movie[]> {
-    return await this.movieService.getAllMovies();
+  async fetchAllMovies(): Promise<RespnseDto> {
+    return new RespnseDto({
+      data: await this.movieService.getAllMovies(),
+    });
   }
 
   @Get()
-  async fetchMovie(@Query('id') id: number): Promise<Movie> {
-    return await this.movieService.getMovieById(id);
+  async fetchMovie(@Query() id: number): Promise<RespnseDto> {
+    return new RespnseDto({
+      data: await this.movieService.getMovieById(id),
+    });
   }
 
   @Post()
-  async saveMovie(@Body() movie: Movie): Promise<Movie> {
-    return await this.movieService.saveMovie(movie);
+  @HttpCode(HttpStatus.CREATED)
+  async saveMovie(@Body() movie: Movie): Promise<RespnseDto> {
+    return new RespnseDto({
+      statusCode: HttpStatus.CREATED,
+      message: 'CREATED',
+      data: await this.movieService.saveMovie(movie),
+    });
   }
 
   @Put()
-  async updateMovie(@Query() id: number, @Body() movie: Movie): Promise<Movie> {
-    return await this.movieService.updateMovie(id, movie);
+  async updateMovie(
+    @Query() id: number,
+    @Body() movie: Movie,
+  ): Promise<RespnseDto> {
+    return new RespnseDto({
+      data: await this.movieService.updateMovie(id, movie),
+    });
   }
 
   @Delete()
-  async deleteMovie(@Query() id: number): Promise<void> {
+  async deleteMovie(@Query() id: number): Promise<RespnseDto> {
     await this.movieService.removeMovieById(id);
+
+    return new RespnseDto({ message: 'Movie removed' });
   }
 }
